860d9324f3b8fd1c4684009ffbb227982074dba2
John Coppola <jdcoppola@gmail.com>
2018-01-28 15:01:48 -0500

    creates app architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { EmployeeItemComponent } from './employees/employee-list/employee-item/employee-item.component';
+import { EmployeeDetailsComponent } from './employees/employee-details/employee-details.component';
+import { CandidatesComponent } from './candidates/candidates.component';
+import { CandidateListComponent } from './candidates/candidate-list/candidate-list.component';
+import { CandidateItemComponent } from './candidates/candidate-list/candidate-item/candidate-item.component';
+import { CandidateDetailsComponent } from './candidates/candidate-details/candidate-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmployeesComponent,
+    EmployeeListComponent,
+    EmployeeItemComponent,
+    EmployeeDetailsComponent,
+    CandidatesComponent,
+    CandidateListComponent,
+    CandidateItemComponent,
+    CandidateDetailsComponent
   ],
   imports: [
     BrowserModule
