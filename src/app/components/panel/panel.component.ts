4feb029ef83a01619d6d90eed489e804d6ebbb37
John Coppola <jcoppola@instien.com>
2018-02-09 18:08:19 -0500

    adds icons, tidies UI, adds edit btn

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PanelComponent implements OnInit {
   @Input() title: string;
+  @Input() center: boolean;
 
   constructor() { }
 
